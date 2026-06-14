07c13603c7a4c52c6424e69c2968c931b0be9efa
Sergio Marquez <smarquez15@gmail.com>
2026-06-15 00:28:14 +0200

    fix ArtistCard ?. to images

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -5,7 +5,7 @@ const ArtistCard = ({name, images, genres, popularity}) => {
   return (
     <div>
         <h1>Artista {name}</h1>
-        <img src={images?.[0].url} />
+        <img src={images?.[0]?.url} />
         <p>{genres?.[0] ? genres[0] : 'no data'}</p>
         <p>{popularity}</p>
     </div>
