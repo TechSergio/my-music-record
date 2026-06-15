3c4922cb98af8d8fe5645044252244a1e6615f3d
Sergio Marquez <smarquez15@gmail.com>
2026-06-15 23:16:23 +0200

    refactor: improve design of artist cards

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -2,10 +2,14 @@
 const ArtistCard = ({name, images, rank}) => {
 
   return (
-    <div>
-        <h1>Artista {name}</h1>
-        <img src={images?.[0]?.url} />
-        <p>{rank}</p>
+    <div className="rounded-2xl overflow-hidden relative aspect-square">
+        <img className='absolute inset-0 object-cover w-full h-full' src={images?.[0]?.url} />
+        <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent p-3 h-3/5 flex items-end gap-2">
+            <span className="text-white rounded-lg px-1.5 text-4xl ">{rank}</span>
+            <h2 className="text-white font-semibold truncate pb-2"> {name}</h2>
+        </div>
+
+
         {/* Se encuentra deprecado
         <p>{genres?.[0] ? genres[0] : 'no data'}</p> 
         <p>{popularity}</p>*/}
