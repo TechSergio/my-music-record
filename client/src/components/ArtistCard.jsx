c8e039bbb923129b9823fb2a5f10634ebf43517d
Sergio Marquez <smarquez15@gmail.com>
2026-06-15 21:37:15 +0200

    refactor: add rank to TopArtists, simplify ArtistCard

--- client/src/components/ArtistCard.jsx
+++ client/src/components/ArtistCard.jsx
@@ -1,12 +1,15 @@
 
-const ArtistCard = ({name, images, genres, popularity}) => {
+const ArtistCard = ({name, images, rank}) => {
 
   return (
     <div>
         <h1>Artista {name}</h1>
         <img src={images?.[0]?.url} />
-        <p>{genres?.[0] ? genres[0] : 'no data'}</p>
-        <p>{popularity}</p>
+        <p>{rank}</p>
+        {/* Se encuentra deprecado
+        <p>{genres?.[0] ? genres[0] : 'no data'}</p> 
+        <p>{popularity}</p>*/}
+        
     </div>
   )
 }
--- client/src/components/TopArtists.jsx
+++ client/src/components/TopArtists.jsx
@@ -10,21 +10,20 @@ const TopArtists = () => {
               //funcion async para pedir datos al backend
           const loadArtists = async ()=> {
           const data = await fetchFromApi('/api/top-artists')
-          console.log(data)
           setArtists(data.items)
         }
-
+        
         loadArtists()
         
     }, [])
-          console.log(artists)
+
 
   return (
     <div>
       <h1>TopArtists</h1>
       <ul className="grid  md:grid-cols-3 lg:grid-cols-4 gap-2">
-        {artists.map((artist) => {
-          return <ArtistCard key={artist.id} {...artist} />
+        {artists.map((artist, index) => {
+          return <ArtistCard key={artist.id} {...artist} rank={index + 1}/>
         }
       )}
       </ul>
